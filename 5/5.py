4d353461254114a768ba5f8c01f9143c36a8704e
Jack Haden-Enneking <jacktose@users.noreply.github.com>
2022-01-06 22:25:11 -0800

    day 5 part 2

--- 5/5.py
+++ 5/5.py
@@ -12,16 +12,16 @@ from collections import defaultdict
 def main():
     data = get_input()
     print('part 1:')
-    print(part_1(data))
-    #print('\npart 2:')
-    #print(part_2(data))
+    print(part_1(data, print_grid=True))
+    print('\npart 2:')
+    print(part_2(data, print_grid=True))
 
 def get_input(file='./input'):
     with open(file, 'r') as f:
         data = f.read().splitlines()
     return data
 
-def part_1(data):
+def part_1(data, print_grid=False):
     '''Rectalinear lines only'''
     lines = []
     for linestring in data:
@@ -29,17 +29,15 @@ def part_1(data):
         if line.horizontal or line.vertical:
             lines.append(line)
     grid = Grid(lines)
-    print(grid)
+    if print_grid: print(grid)
     return grid.intersections()
 
-def part_2(data):
+def part_2(data, print_grid=False):
     ''''''
     lines = [Line(linestring) for linestring in data]
-    x_max = max(line.right for line in lines)
-    y_max = max(line.bottom for line in lines)
-    print(f'{x_max=}, {y_max=}')
-    
-    return intersections(grid)
+    grid = Grid(lines)
+    if print_grid: print(grid)
+    return grid.intersections()
 
 
 class Line:
@@ -49,8 +47,12 @@ class Line:
         self.x1, self.y1, self.x2, self.y2 = (int(n) for n in re.split(r'\D+', linestring))
     
     def __repr__(self):
+        '''Line('491,392 -> 34,392')'''
+        return f"Line('{self.__str__()}')"
+    
+    def __str__(self):
         '''491,392 -> 34,392'''
-        return f"Line('{self.x1},{self.y1} -> {self.x2},{self.y2}')"
+        return f'{self.x1},{self.y1} -> {self.x2},{self.y2}'
     
     @property
     def horizontal(self):
@@ -63,6 +65,17 @@ class Line:
     @property
     def diagonal(self):
         return not self.horizontal and not self.vertical
+    
+    @property
+    def slope(self):
+        if self.horizontal:
+            return 0
+        elif self.vertical:
+            return None
+        elif (self.x1 < self.x2) == (self.y1 < self.y2):
+            return -1
+        elif (self.x1 < self.x2) != (self.y1 < self.y2):
+            return 1
     
     @property
     def left(self):
@@ -80,29 +93,54 @@ class Line:
     def bottom(self):
         return max(self.y1, self.y2)
     
-    def contains(self, x, y):
-        '''ASSUME RECTALINEAR'''
+    #@cached_property
+    @property
+    def points(self):
         if self.horizontal:
-            return y == self.y1 and x >= self.left and x <= self.right
+            return tuple((x,self.y1) for x in ri_range(self.x1, self.x2))
         elif self.vertical:
-            return x == self.x1 and y >= self.top and y <= self.bottom
+            return tuple((self.x1,y) for y in ri_range(self.y1, self.y2))
         else:
-            raise NotImplementedError('can only do rectalinear for now')
+            return tuple(zip(
+                ri_range(self.x1, self.x2),
+                ri_range(self.y1, self.y2)
+            ))
 
 
 class Grid:
     ''''''
     def __init__(self, lines):
+        # defaultdict is sparse (efficient), point value is effectively 0 until set
+        # access with Grid.grid[x][y]
         self.grid = defaultdict(lambda: defaultdict(lambda: 0))
         for line in lines:
-            if line.horizontal:
-                for x in range(line.left, line.right+1):
-                    self.grid[x][line.y1] += 1
-            elif line.vertical:
-                for y in range(line.top, line.bottom+1):
-                    self.grid[line.x1][y] += 1
-            else:
-                raise NotImplementedError('can only do rectalinear for now')
+            for x, y in line.points:
+                self.grid[x][y] += 1
+        #breakpoint()
+    
+    def __str__(self):
+        '''as in the example'''
+        string = ''
+        x_len, y_len = self.size()
+        # can't really use a comprehension because first order is columns
+        for y in range(y_len):
+            for x in range(x_len):
+                #string += '.' if (self.grid[x][y] == 0) else self.grid[x][y], end=''
+                match self.grid[x][y]:
+                    case 0:
+                        string += '.'
+                    case 1:
+                        string += '1'
+                    case 2:
+                        string += f'{c.bold}{c.blue}2{c.reset}'
+                    case 3:
+                        string += f'{c.bold}{c.green}3{c.reset}'
+                    case 4:
+                        string += f'{c.bold}{c.yellow}4{c.reset}'
+                    case n if n > 4:
+                        string += f'{c.bold}{c.red}{n}{c.reset}'
+            string += '\n'
+        return string.strip()
     
     def size(self):
         x_max = max(max(y.keys() for y in self.grid.values()))
@@ -117,30 +155,6 @@ class Grid:
                 if n_lines > 1:
                     intersections += 1
         return intersections
-    
-    def __str__(self):
-        '''as in the example'''
-        string = ''
-        x_len, y_len = self.size()
-        # can't really use a comprehension because first order is columns
-        for x in range(x_len):
-            for y in range(y_len):
-                #string += '.' if (self.grid[x][y] == 0) else self.grid[x][y], end=''
-                match self.grid[x][y]:
-                    case 0:
-                        string += '.'
-                    case 1:
-                        string += '1'
-                    case 2:
-                        string += f'{c.bold}{c.blue}2{c.reset}'
-                    case 3:
-                        string += f'{c.bold}{c.green}3{c.reset}'
-                    case 4:
-                        string += f'{c.bold}{c.yellow}4{c.reset}'
-                    case n if n > 4:
-                        string += f'{c.bold}{c.red}5{c.reset}'
-            string += '\n'
-        return string.strip()
 
 
 class c:
@@ -157,6 +171,17 @@ class c:
     blue =      '\033[94m'
     purple =    '\033[95m'
     cyan =      '\033[96m'
+
+
+def ri_range(start, stop):
+    '''Reversible, Inclusive range
+    R: Make the range run backwards if it wants to!
+    I: Go the extra step
+    '''
+    if stop >= start:
+        return range(start, stop+1, 1)
+    else:
+        return range(start, stop-1, -1)
 
 
 if __name__ == '__main__':
