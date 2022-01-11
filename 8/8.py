0fe3df70aa7ee9116de55590105b170c17e8c691
Jack Haden-Enneking <jacktose@users.noreply.github.com>
2022-01-10 17:42:29 -0800

    day 8 part 2

--- 8/8.py
+++ 8/8.py
@@ -6,17 +6,27 @@ Day 8: Seven Segment Search
 """
 
 import sys
-#import itertools
 
 def main():
-    print('example:')
-    ex_data = get_input('./example')
-    print(part_1(ex_data))
+    print('example 1:')
+    ex1_data = [( ('acedgfb', 'cdfbe', 'gcdfa', 'fbcad', 'dab',
+                   'cefabd', 'cdfgeb', 'eafb', 'cagedb', 'ab'),
+                  ('cdfeb', 'fcadb', 'cdfeb', 'cdbaf') )]
+    ex2_data = get_input('./example')
+    print(part_1(ex2_data))
+    
     print('\npart 1:')
     data = get_input()
     print(part_1(data))
-    #print('\npart 2:')
-    #print(part_2(data))
+    
+    print('\nexample 2.1:')
+    print(part_2(ex1_data, debug=True))
+    
+    print('\nexample 2.2:')
+    print(part_2(ex2_data, debug=True))
+    
+    print('\npart 2:')
+    print(part_2(data))
     #breakpoint()
 
 def get_input(file='./input'):
@@ -39,8 +49,90 @@ def part_1(data):
     # ↑ is faster than comprehensions ↓
     #return sum([len(digit) in (2,3,4,7) for digit in d[1]].count(True) for d in data)
     #return len(list(filter(lambda s: len(s) in (2,3,4,7), itertools.chain.from_iterable(d[1] for d in data))))
-    #breakpoint()
     return count
+
+def part_2(data, debug=False):
+    '''For each entry, determine all of the wire/segment
+    connections and decode the four-digit output values.
+    What do you get if you add up all of the output values?
+    '''
+    total = 0
+    for signal, output_scrambled in data:
+        key = key_from_sigs(signal)
+        output_digits = (str(decode(pattern, key)) for pattern in output_scrambled)
+        output_number = int(''.join(str(d) for d in output_digits))
+        if debug:
+            print(f'{" ".join(output_scrambled)} ->\t{output_number}')
+        total += output_number
+    return total
+
+def key_from_sigs(signals):
+    '''from signals like:
+    ('gbcefa', 'eac', 'acfbg', 'ae', 'dcabfg', 'begcdaf', 'ecgba', 'fgaedc', 'beaf', 'gcbde')
+    to true segments:
+     aaaa 
+    b    c
+    b    c
+     dddd 
+    e    f
+    e    f
+     gggg 
+    '''
+    patterns = [set(p) for p in signals]
+    digits = {}
+    digits[235] = []
+    digits[690] = []
+    
+    for p in patterns:
+        match len(p):
+            case 2:  # 1
+                digits[1] = p
+            case 3:  # 7
+                digits[7] = p
+            case 4:  # 4
+                digits[4] = p
+            case 5:  # 2, 3, or 5
+                digits[235].append(p)
+            case 6:  # 0, 6, or 9
+                digits[690].append(p)
+            case 7:  # 8
+                digits[8] = p
+    
+    segs = {}
+    segs['cf'] = digits[1]
+    segs['acf'] = digits[7]
+    segs['bcdf'] = digits[4]
+    segs['abcdefg'] = digits[8]
+    segs['adg'] = set.intersection(*digits[235])
+    segs['abfg'] = set.intersection(*digits[690])
+    segs['a'] = segs['acf'] - segs['cf']
+    segs['b'] = segs['abfg'] - segs['acf'] - segs['adg']
+    segs['c'] = segs['cf'] - segs['abfg']
+    segs['d'] = segs['adg'] - segs['abfg']
+    segs['e'] = segs['abcdefg'] - segs['abfg'] - segs['bcdf']
+    segs['f'] = segs['cf'] - segs['c']
+    segs['g'] = segs['adg'] - segs['a'] - segs['d']
+    
+    segs = {s: segs[s].pop() for s in 'abcdefg'}
+    segtrans = {v:k for k,v in segs.items()}
+    return str.maketrans(segtrans)
+
+_digit = {
+    'abcefg':   0,
+    'cf':       1,
+    'acdeg':    2,
+    'acdfg':    3,
+    'bcdf':     4,
+    'abdfg':    5,
+    'abdefg':   6,
+    'acf':      7,
+    'abcdefg':  8,
+    'abcdfg':   9,
+}
+def decode(segments_scrambled, key):
+    '''e.g. 'gbcefa' -> 'abdefg' -> 6'''
+    segments = ''.join(sorted(segments_scrambled.translate(key)))
+    return _digit[segments]
 
 
 if __name__ == '__main__':
