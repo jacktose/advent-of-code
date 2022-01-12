c07aaf682147141ad324c868ad4a4f0f3a7719fe
Jack Haden-Enneking <jacktose@users.noreply.github.com>
2022-01-11 17:03:49 -0800

    day 9 improve
    tyyyyyype hiiiiiiintiiing!

--- 9/9.py
+++ 9/9.py
@@ -7,6 +7,7 @@ Day 9: Smoke Basin
 
 import sys
 import math
+from collections.abc import Generator, Iterable
 
 def main():
     ex_data = get_input('./example')
@@ -22,20 +23,20 @@ def main():
     print(part_2(ex_data, debug=True), '= 1134?')
     
     print('\npart 2:')
-    print(part_2(data, debug=True))
+    print(part_2(data, debug=True), '= 1235430?')
 
-def get_input(file='./input'):
+def get_input(file='./input') -> tuple[tuple[int, ...], ...]:
     with open(file, 'r') as f:
         data = tuple(tuple(int(n) for n in line) for line in f.read().splitlines())
     return data
 
-def part_1(data, debug=False):
+def part_1(data: tuple[tuple[int, ...], ...], debug=False) -> int:
     '''What is the sum of the risk levels of all low points on your heightmap?'''
     floor = Map(data)
     if debug: print('low points:', list(floor.low_points()))
     return floor.risk()
 
-def part_2(data, debug=False):
+def part_2(data: tuple[tuple[int, ...], ...], debug=False) -> int:
     '''What do you get if you multiply together the sizes of the three largest basins?'''
     floor = Map(data)
     if debug:
@@ -47,23 +48,23 @@ def part_2(data, debug=False):
 
 class Map:
     '''2D array of seafloor, with puzzling funcs'''
-    def __init__(self, points):
-        self.points = points  # 2D array as tuple of tuples of ints
-        self.height = len(self.points)
-        self.width = len(self.points[0])
+    def __init__(self, points: tuple[tuple[int, ...], ...]) -> None:
+        self.points: tuple[tuple[int, ...], ...] = points  # 2D array as tuple of tuples of ints
+        self.height: int = len(self.points)
+        self.width: int = len(self.points[0])
     
-    def low_points(self):
+    def low_points(self) -> Generator[tuple[int, int, int], None, None]:
         '''generator of low points on map'''
         for row in range(self.height):
             for col in range(self.width):
                 if self._is_low_point(row, col):
                     yield (row, col, self.points[row][col])
     
-    def _is_low_point(self, row, col):
+    def _is_low_point(self, row: int, col: int, _=None) -> bool:
         '''is it lower than all 2–4 orthogonal neighbors?'''
         return all(self.points[row][col] < a[2] for a in self.adj(row, col))
     
-    def adj(self, row, col):
+    def adj(self, row: int, col: int, _=None) -> Generator[tuple[int, int, int], None, None]:
         '''generator of orthogonal neighbors'''
         for r, c in ((row-1, col),  # north
                      (row+1, col),  # south
@@ -72,16 +73,16 @@ class Map:
             if (0 <= r < self.height) and (0 <= c < self.width):
                 yield (r, c, self.points[r][c])
     
-    def risk(self):
+    def risk(self) -> int:
         '''The risk level of a low point is 1 plus its height.'''
         return sum(p[2]+1 for p in self.low_points())
     
-    def basins(self):
+    def basins(self) -> Generator[int, None, None]:
         '''generator of sizes of basins'''
         for low_point in self.low_points():
             yield self._basin_size(low_point)
     
-    def _basin_size(self, initial_point: tuple):
+    def _basin_size(self, initial_point: tuple[int, int, int]) -> int:
         '''given a (low?) point, count the points in its basin'''
         basin_maybe = {initial_point}
         basin_yes = set()
@@ -89,7 +90,7 @@ class Map:
             p = basin_maybe.pop()
             if p[2] < 9:  # 9s are basin borders and not included
                 basin_yes.add(p)
-                basin_maybe.update(p for p in self.adj(p[0], p[1]) if p not in basin_yes)
+                basin_maybe.update(p for p in self.adj(*p) if p not in basin_yes)
         return len(basin_yes)
 
 
