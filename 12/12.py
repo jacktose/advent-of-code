2611f978954be2579aec03866152a24b26ef07ad
Jack Haden-Enneking <jacktose@users.noreply.github.com>
2022-01-16 15:07:33 -0800

    day 12 part 2

--- 12/12.py
+++ 12/12.py
@@ -13,23 +13,29 @@ def main():
     ex3_data = get_input('./ex3')
     data = get_input('./input')
     
-    print('example 1:')
+    print('example 1.1:')
     print(part_1(ex1_data, debug=True), '= 10?')
     
-    print('example 2:')
+    print('\nexample 1.2:')
     print(part_1(ex2_data, debug=True), '= 19?')
     
-    print('example 3:')
+    print('\nexample 1.3:')
     print(part_1(ex3_data, debug=False), '= 226?')
     
     print('\npart 1:')
     print(part_1(data), 'paths')
     
-    #print('\nexample 2:')
-    #print(part_2(ex_data))
+    print('\nexample 2.1:')
+    print(part_2(ex1_data, debug=True), '= 36?')
     
-    #print('\npart 2:')
-    #print(part_2(data))
+    print('\nexample 2.2:')
+    print(part_2(ex2_data, debug=False), '= 103?')
+    
+    print('\nexample 2.3:')
+    print(part_2(ex3_data, debug=False), '= 3509?')
+    
+    print('\npart 2:')
+    print(part_2(data), 'paths')
     #breakpoint()
 
 def get_input(file='./input'):
@@ -39,32 +45,62 @@ def get_input(file='./input'):
 
 def part_1(data, debug=False):
     '''How many paths through this cave system are there that visit small caves at most once?'''
-    cave_set = set()
-    for d in data:
-        cave_set.update(d)
-    caves = {}
-    for cave in cave_set:
-        caves[cave] = {c2 for c1, c2 in data if c1 == cave}
-        caves[cave].update(c1 for c1, c2 in data if c2 == cave)
-    paths = find_paths(caves, path=['start'])
-    if debug:
-        for path in paths:
-            print(','.join(path))
+    caves = caves_from_paths(data)
+    paths = find_paths_1(caves, path=['start'])
+    if debug: print_paths(paths)
     return len(paths)
 
-def find_paths(caves, path=['start']):
+def part_2(data, debug=False):
+    '''a single small cave can be visited at most twice, and the remaining small caves can be visited at most once'''
+    caves = caves_from_paths(data)
+    paths = find_paths_2(caves, path=['start'])
+    if debug: print_paths(paths)
+    return len(paths)
+
+def caves_from_paths(paths):
+    '''Take tuples defining paths, make dict of caves: neighbors'''
+    caves = {}
+    for cave in set.union(*(set(p) for p in paths)):
+        caves[cave] = {c2 for c1, c2 in paths if c1 == cave}
+        caves[cave].update(c1 for c1, c2 in paths if c2 == cave)
+    return caves
+
+def find_paths_1(caves, path=['start']):
     if path[-1] == 'end':
         return [path]
     paths = []
     for neighbor in caves[path[-1]]:
         if neighbor.isupper() or neighbor not in path:
-            paths.extend(find_paths(caves, path+[neighbor]))
+            paths.extend(find_paths_1(caves, path+[neighbor]))
     return paths
 
+def find_paths_2(caves, path=['start']):
+    if path[-1] == 'end':
+        return [path]
+    paths = []
+    for neighbor in caves[path[-1]]:
+        if (
+            neighbor.isupper()
+            or neighbor not in path
+            or (neighbor != 'start'
+                and not has_double_small(path))
+        ):
+            paths.extend(find_paths_2(caves, path+[neighbor]))
+    return paths
 
+def has_double_small(path):
+    '''Have we already visited some small cave twice?'''
+    for i, c in enumerate(path):
+        if c.islower() and c in path[i+1:]:
+            return True
+    else:
+        return False
 
-def part_2(data):
-    '''assignment'''
+def print_paths(paths):
+    '''Output like in examples'''
+    print('\n'.join(
+        ','.join(path) for path in paths
+    ))
 
 
 if __name__ == '__main__':
