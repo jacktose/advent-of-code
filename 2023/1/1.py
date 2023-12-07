aafb1feb86db6ee9fbdf2faefaee752fd2c4dda5
Jack Haden-Enneking <jacktose@users.noreply.github.com>
2023-12-06 16:05:28 -0800

    day 1 part 2
    If it's stupid and it words, it's not stupid.

--- 2023/1/1.py
+++ 2023/1/1.py
@@ -2,26 +2,27 @@
 
 """
 https://adventofcode.com/2021/day/1
-
+Day 1: Trebuchet?!
 """
 
 import re
 
 def main():
-    ex_data = get_input('./example')
+    ex_data_1 = get_input('./example_1')
+    ex_data_2 = get_input('./example_2')
     data = get_input('./input')
     
     print('example 1:')
-    print(part_1(ex_data), '= 142?')
+    print(part_1(ex_data_1), '= 142?')
     
     print('\npart 1:')
     print(part_1(data))
     
-    #print('\nexample 2:')
-    #print(part_2(ex_data), '= _?')
+    print('\nexample 2:')
+    print(part_2(ex_data_2), '= 281?')
     
-    #print('\npart 2:')
-    #print(part_2(data))
+    print('\npart 2:')
+    print(part_2(data))
 
     #breakpoint()
 
@@ -39,7 +40,37 @@ def part_1(data):
     return sum
 
 def part_2(data):
-    '''assignment'''
+    '''What is the sum of all of the calibration values?'''
+    digitize = {
+        #'0': 0, 'zero': 0,
+        '1': 1, 'one': 1,
+        '2': 2, 'two': 2,
+        '3': 3, 'three': 3,
+        '4': 4, 'four': 4,
+        '5': 5, 'five': 5,
+        '6': 6, 'six': 6,
+        '7': 7, 'seven': 7,
+        '8': 8, 'eight': 8,
+        '9': 9, 'nine': 9,
+    }
+    digitize_rev = {
+        #'0': 0, 'orez': 0,
+        '1': 1, 'eno': 1,
+        '2': 2, 'owt': 2,
+        '3': 3, 'eerht': 3,
+        '4': 4, 'ruof': 4,
+        '5': 5, 'evif': 5,
+        '6': 6, 'xis': 6,
+        '7': 7, 'neves': 7,
+        '8': 8, 'thgie': 8,
+        '9': 9, 'enin': 9,
+    }
+    sum = 0
+    for line in data:
+        digit_1 = re.search(r'\d|one|two|three|four|five|six|seven|eight|nine', line).group(0)
+        digit_2 = re.search(r'\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin', line[::-1]).group(0)
+        sum += (10 * digitize[digit_1]) + (1 * digitize_rev[digit_2])
+    return sum
 
 
 if __name__ == '__main__':
