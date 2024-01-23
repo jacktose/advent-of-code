ffc307876ebf0db6e798017520fa9a36429392d0
Jack Haden-Enneking <jacktose@users.noreply.github.com>
2024-01-23 10:42:49 -0800

    objectify

--- 2023/14/14.py
+++ 2023/14/14.py
@@ -5,7 +5,6 @@ https://adventofcode.com/2021/day/14
 Day 14: Parabolic Reflector Dish
 """
 
-from functools import singledispatch
 from typing import Sequence
 
 def main():
@@ -34,15 +33,8 @@ def part_1(data):
     Afterward, what is the total load on the north support beams?'''
     return sum(load_e_row(row) for row in rotate_cw(tuple(data)))
 
-@singledispatch
-def rotate_cw(grid):
-    raise TypeError('rotate_cw takes tuple or list')
-@rotate_cw.register
-def _(grid: tuple) -> tuple[str, ...]:
+def rotate_cw(grid: tuple[str, ...]) -> tuple[str, ...]:
     return tuple(''.join(_) for _ in zip(*reversed(grid)))
-@rotate_cw.register
-def _(grid: list) -> list[list[str]]:
-    return [list(_) for _ in zip(*reversed(grid))]
 
 def load_e_grid(grid):
     return sum(load_e_row(row) for row in rotate_cw(grid))
@@ -64,44 +56,49 @@ def load_e_row(row: str) -> int:
 def part_2(data: Sequence[Sequence[str]], cycles: int = 1_000_000_000) -> int:
     '''Run the spin cycle for 1000000000 cycles: North, West, South, East
     Afterward, what is the total load on the north support beams?'''
-    grid: list[list[str]] = [list(row_str) for row_str in data]
+    grid = Grid(data)
     cache = {}
     i = 0; i_max = 4 * cycles
     while i < i_max:
-        grid = rotate_cw(grid)
-        tilt_e_grid(grid)  # in place
-        grid_str = '\n'.join(''.join(row) for row in grid)
-        if grid_str in cache:
+        grid.rotate_cw()
+        grid.tilt_e()
+        if (grid_str := str(grid)) in cache:
             period = i - cache[grid_str]
-            i = i_max - ((i_max - i) % period)
+            remaining = i_max - i
+            i = i_max - (remaining % period)
         else:
             cache[grid_str] = i
         i += 1
-    return load_n_grid(grid)
+    return grid.load_n
 
-def tilt_e_grid(grid: list[list[str]]) -> None:
-    for row in grid:
-        tilt_e(row)  # in place
-    return None
+class Grid:
+    def __init__(self, grid: Sequence[Sequence[str]]):
+        self._grid: list[list[str]] = [list(row) for row in grid]
+    def __str__(self) -> str:
+        return '\n'.join(''.join(row) for row in self._grid)
 
-def tilt_e(row: list[str]) -> None:
-    target = len(row) - 1
-    for i, c in etaremune(row):
-        if c == 'O':
-            row[i] = '.'
-            row[target] = 'O'
-            target -= 1
-        elif c == '#':
-            target = i - 1
-    return None
+    def rotate_cw(self) -> None:
+        self._grid = [list(_) for _ in zip(*reversed(self._grid))]
+
+    @property
+    def load_n(self) -> int:
+        return sum(i * row.count('O') for (i, row) in enumerate(self._grid[::-1], start=1))
+
+    def tilt_e(self) -> None:
+        for row in self._grid:
+            target = len(row) - 1
+            for i, c in etaremune(row):
+                if c == 'O':
+                    row[i] = '.'
+                    row[target] = 'O'
+                    target -= 1
+                elif c == '#':
+                    target = i - 1
 
 def etaremune(sequence: Sequence):
     '''enumerate backward'''
     for n in reversed(range(len(sequence))):
         yield n, sequence[n]
-
-def load_n_grid(grid) -> int:
-    return sum(i * row.count('O') for (i, row) in enumerate(grid[::-1], start=1))
     
 
 if __name__ == '__main__':
