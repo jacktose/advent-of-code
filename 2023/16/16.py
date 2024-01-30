0d20bd9fc77aeeceec904401ab49648c6f511752
Jack Haden-Enneking <jacktose@users.noreply.github.com>
2024-01-30 15:39:51 -0800

    day 16 clean up & objectify

--- 2023/16/16.py
+++ 2023/16/16.py
@@ -17,17 +17,16 @@ from typing import NamedTuple, Sequence
 def main():
     ex_data = get_input('./example')
     data = get_input('./input')
-    
+
     print('example 1:')
     print(part_1(ex_data, watch=True), '= 46?')
-    
+
     print('\npart 1:')
     print(part_1(data))
-    #print(part_1(data, watch=True, delay=0))
-    
+
     print('\nexample 2:')
-    print(part_2(ex_data, watch=True, delay=0), '= 51?')
-    
+    print(part_2(ex_data, watch=True, delay=0.01, inter_delay=0.2), '= 51?')
+
     print('\npart 2:')
     print(part_2(data))
 
@@ -41,13 +40,14 @@ def part_1(data, watch: bool = False, delay: float = 0.05) -> int:
     how many tiles end up being energized?'''
     return light_it_up(data, start=Beam(Point(0, 0), Velocity(0, 1)), watch=watch, delay=delay)
 
-def part_2(data, watch: bool = False, delay: float = 0.05) -> int:
+def part_2(data, watch: bool = False, delay: float = 0.05, inter_delay: float = 0) -> int:
     '''Find the initial beam configuration that energizes the largest number of tiles;
     how many tiles are energized in that configuration?'''
     n_rows = len(data)
     n_cols = len(data[0])
     return max(
-        light_it_up(data, start=Beam(p, v), watch=watch, delay=delay)
+        light_it_up(data, start=Beam(p, v), watch=watch,
+                    delay=delay, inter_delay=inter_delay)
         for p, v in itertools.chain(
             ((Point(0,        c), Velocity( 1,  0)) for c in range(n_cols)),
             ((Point(n_rows-1, c), Velocity(-1,  0)) for c in range(n_cols)),
@@ -56,17 +56,22 @@ def part_2(data, watch: bool = False, delay: float = 0.05) -> int:
         )
     )
 
-def light_it_up(data, start: Beam, watch: bool = False, delay: float = 0.05) -> int:
+def light_it_up(data, start: Beam, watch: bool = False,
+                delay: float = 0.05, inter_delay: float = 0) -> int:
     beams = deque([start])
     lit = [[Direction(0) for p in row] for row in data]
     if watch:
-        display_init(len(data))
+        display = Display(data)
+        display.highlight = [beams[0].loc]
+        sleep(inter_delay)
+    else:
+        display = None
     while beams:
-        # Refresh display after we've cycled all the beams.
-        if watch:
-            display_back_up(data)
-            display(data, lit, beams)
+        # Refresh display every cycle
+        if display:
+            display.print(lit, beams)
             sleep(delay)
+
         # Split/reflect all beams
         for _ in range(len(beams)):
             beam = beams.popleft()
@@ -79,6 +84,7 @@ def light_it_up(data, start: Beam, watch: bool = False, delay: float = 0.05) -> 
             # Interact with tile
             tile = data[p.r][p.c]
             beams.extend(b for b in beam.reflected(tile) if b not in beams)
+
         # Move all beams
         for _ in range(len(beams)):
             beam = beams.popleft()
@@ -86,13 +92,17 @@ def light_it_up(data, start: Beam, watch: bool = False, delay: float = 0.05) -> 
             if in_bounds(data, beam.loc):
                 beams.append(beam)
                 # Else let it die
+
     return sum(sum(tile != Direction.NONE for tile in row) for row in lit)
+
+def in_bounds(data: Sequence[Sequence], point: Point) -> bool:
+    return (0 <= point.r < len(data)) and (0 <= point.c < len(data[0]))
 
 class classproperty:
     def __init__(self, func):
         self._func = func
     def __get__(self, obj, owner):
-        return self._func(owner)        
+        return self._func(owner)
 
 class Direction(enum.Flag):
     NONE = 0
@@ -102,10 +112,7 @@ class Direction(enum.Flag):
     E = enum.auto()
     @classproperty
     def ALL(cls):
-        return ~cls(0)
-
-def in_bounds(data: Sequence[Sequence], point: Point) -> bool:
-    return (0 <= point.r < len(data)) and (0 <= point.c < len(data[0]))
+        return ~cls(0)  # type: ignore
 
 class Point(NamedTuple):
     r: int
@@ -120,7 +127,7 @@ class Point(NamedTuple):
             return Point(self.r + other[0], self.c + other[1])
         else:
             raise TypeError
-            
+
 class Velocity(NamedTuple):
     dr: int
     dc: int
@@ -135,7 +142,6 @@ class Velocity(NamedTuple):
         raise NotImplemented('Cardinal directions only')
 
     def reflected(self, tile: str) -> Velocity:
-        #TODO: Check this logic!
         match tile:
             case '/':
                 return Velocity(dr=-self.dc, dc=-self.dr)
@@ -151,7 +157,7 @@ class Beam:
     def advance(self) -> Point:
         self.loc += self.vel
         return self.loc
-    
+
     def reflected(self, tile: str) -> list[Beam]:
         match tile:
             case '.':
@@ -167,7 +173,7 @@ class Beam:
             case '-':
                 return [Beam(self.loc, self.vel.reflected(t)) for t in '/\\']
         raise ValueError
-    
+
     @property
     def character(self) -> str:
         match self.vel:
@@ -176,36 +182,55 @@ class Beam:
             case (0, -1): return '<'
             case (-1, 0): return '^'
         raise RuntimeError('Unexpected velocity: {self.vel}')
-    
+
     @property
     def direction(self) -> Direction:
         return self.vel.direction
-            
-def display_back_up(data) -> None:
-    print(f'\x1b[{len(data)}F', end='')
 
-def display_init(lines: int):
-    term = os.get_terminal_size()
-    if lines >= term.lines:
-        print(f'Warning: grid len {lines} too tall for terminal {term.columns=} {term.lines=}')
-    print(control.HIDE_CURSOR)
-    print('\n' * lines, end='')
+class Display:
+    def __init__(self, grid) -> None:
+        self.grid = grid
+        self.rows = len(self.grid)
+        self.cols = len(self.grid[0])
+        self.highlight: Sequence[Point] = []
+        term = os.get_terminal_size()
+        if self.rows >= term.lines:
+            print(f'Warning: grid len {self.rows} too tall for terminal {term.columns=} {term.lines=}')
+        self._started = False
 
-def display(data, lit, beams) -> None:
-    beam_chars = {beam.loc: beam.character for beam in beams}
-    for r, row in enumerate(data):
-        for c, tile in enumerate(row):
-            try:
-                print(style.BOLD + style.FG.YELLOW,
-                      beam_chars[Point(r, c)],
-                      style.RESET,
-                      sep= '', end='')
-            except KeyError:
-                print(style.BOLD + style.FG.YELLOW if lit[r][c] else '',
-                      tile,
-                      style.RESET,
-                      sep= '', end='')
-        print()
+    def _prep(self) -> None:
+        print(control.HIDE_CURSOR)
+        print('\n' * (self.rows+1), end='')
+
+    def _back_up(self) -> None:
+        print(f'\x1b[{self.rows}F', end='')
+
+    def print(self, lit, beams) -> None:
+        if not self._started:
+            self._prep()
+            self._started = True
+        self._back_up()
+        beam_chars = {beam.loc: beam.character for beam in beams}
+        for r, row in enumerate(self.grid):
+            for c, tile in enumerate(row):
+                if Point(r, c) in self.highlight:
+                    print(style.BG.GREEN, end='')
+                try:
+                    print(style.BOLD, style.FG.YELLOW,
+                        beam_chars[Point(r, c)],
+                        style.RESET,
+                        sep= '', end='')
+                except KeyError:
+                    print(style.BOLD + style.FG.YELLOW if lit[r][c] else '',
+                        tile,
+                        style.RESET,
+                        sep= '', end='')
+            print()
+        print(n_lit(lit) + len(beams), '\r', sep='', end='')
+        pass
+
+def n_lit(lit) -> int:
+    return sum(sum(d != Direction.NONE for d in row) for row in lit)
 
 
 class _fg(NamedTuple):
