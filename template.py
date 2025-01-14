46bf63abfd80d39226797a1589c8bafe763d8654
Jack Haden-Enneking <jacktose@users.noreply.github.com>
2025-01-13 17:04:06 -0800

    update tooling

--- template.py
+++ template.py
@@ -9,21 +9,17 @@ def main():
     ex_data = get_input('./example')
     data = get_input('./input')
     
-    print('example 1:')
-    print(part_1(ex_data), '= _?')
-    
-    #print('\npart 1:')
-    #print(part_1(data))
-    
-    #print('\nexample 2:')
-    #print(part_2(ex_data), '= _?')
-    
-    #print('\npart 2:')
-    #print(part_2(data))
+    print('example 1:', part_1(ex_data), '= _?')
+    #print('\npart 1:', part_1(data))
+    #print('\nexample 2:', part_2(ex_data), '= _?')
+    #print('\npart 2:', part_2(data))
 
 def get_input(file='./input'):
     with open(file, 'r') as f:
+        #data = f.read()
         data = f.read().splitlines()
+        #data = tuple(tuple(line for line in chunk.splitlines()) for chunk in f.read.split('\n\n'))
+        #data = tuple(tuple(int(n) for n in line.split()) for line in f)
     return data
 
 def part_1(data):
@@ -36,4 +32,3 @@ def part_2(data):
 if __name__ == '__main__':
     import sys
     sys.exit(main())
-
