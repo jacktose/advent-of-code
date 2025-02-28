38e1c68126fa6a9cfe544249f01695259d6738f7
Jack Haden-Enneking <jacktose@users.noreply.github.com>
2025-02-28 00:30:33 -0800

    clean up 2024/19

--- 2024/19/19.py
+++ 2024/19/19.py
@@ -6,16 +6,13 @@ Day 19: Linen Layout
 """
 
 from functools import cache
-#import time
 
 def main():
     ex_data = get_input('./example.txt')
     data = get_input('./input.txt')
     
     print('example 1:', part_1(ex_data), '= 6?')
-    #start = time.time()
     print('\npart 1:', part_1(data))  # 300
-    #end = time.time(); print(end - start)
     print('\nexample 2:', part_2(ex_data), '= 16?')
     print('\npart 2:', part_2(data))  # 624802218898092
 
@@ -30,33 +27,16 @@ def part_1(data) -> int:
     '''How many designs are possible?'''
     patterns, designs = data
     patterns = sorted(patterns, key=len, reverse=True)  # Try from longest to shortest
-    # TODO: More preprocessing patterns?
-    #       Does it help to remove long patterns that comprise shorter patterns?
-    #       Or are they effectively just precomputed cache?
 
-    # re.fullmatch() is too slow for this. Needs caching on subproblems to speed backtracking.
     @cache
     def match(design: str) -> bool:
         if design == '':
             return True
-        for pattern in patterns:
-            if design.startswith(pattern):
-                if match(design[len(pattern):]):
-                    return True
-                # else continue (backtrack)
-        else:
-            return False
+        return any(design.startswith(pattern)
+                   and match(design[len(pattern):])
+                   for pattern in patterns)
 
     return sum(1 for design in designs if match(design))
-    #answer: int = 0
-    #for i, design in enumerate(designs):
-    #    print(i, design, sep='\t', end='\t')
-    #    if match(design):
-    #        answer += 1
-    #        print('✅')
-    #    else:
-    #        print('❌')
-    #return answer
 
 def part_2(data) -> int:
     '''Add up the number of different ways you could make each design'''
@@ -66,11 +46,9 @@ def part_2(data) -> int:
     def ways(design: str) -> int:
         if design == '':
             return 1
-        count = 0
-        for pattern in patterns:
-            if design.startswith(pattern):
-                count += ways(design[len(pattern):])
-        return count
+        return sum(ways(design[len(pattern):])
+                   for pattern in patterns
+                   if design.startswith(pattern))
 
     return sum(map(ways, designs))
 
